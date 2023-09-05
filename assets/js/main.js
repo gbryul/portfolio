65545f30ae7842832116cc0bc6bb74de02a202f0
gbryul <gabriel.unionjack@gmail.com>
2023-09-05 17:25:25 -0300

    preenchimento dos dados do portfólio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-function resolveProfileInfo(profileData){
+function resolveProfileInfo(profileData) {
     const name = document.getElementById('profile.name')
     name.innerText = profileData.name
     const job = document.getElementById('profile.job')
@@ -11,19 +11,33 @@ function resolveProfileInfo(profileData){
     const email = document.getElementById('profile.email')
     email.innerText = profileData.email
     email.href = `mailto:${profileData.email}`
-} 
+}
 
-function addSoftSkills(profileData){
+function addSoftSkills(profileData) {
     const softSkills = document.getElementById('profile.skills.softSkills')
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
 }
 
-function addHardSkills(profileData){
+function addHardSkills(profileData) {
     const hardSkills = document.getElementById('profile.skills.hardSkills')
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src = "${skill.logo}" alt = "${skill.nome}" title = "${skill.nome}"></li>`).join('')
 
 }
 
+function addLanguages(profileData) {
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.idiomas.map(languages => `<li>${languages.nome} - Nível: ${languages.nivel}</li>`).join('')
+}
+
+function addPortfolio(profileData) {
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(portfolio => `<li>
+    <h3 class="p-content__portfolio github">${portfolio.nome}</h3>
+    <a href="${portfolio.gitHub}" target="_blank">${portfolio.gitHub}</a>
+    <a href="${portfolio.certificado}" target = "_blank">Certificado</a>
+</li>`).join('')
+
+}
 
 
 (async () => {
@@ -31,4 +45,6 @@ function addHardSkills(profileData){
     resolveProfileInfo(profileData)
     addSoftSkills(profileData)
     addHardSkills(profileData)
+    addLanguages(profileData)
+    addPortfolio(profileData)
 })()
