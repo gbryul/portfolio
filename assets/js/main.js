6d589398ca70c37a9af02eadd044b727b59b07f9
gbryul <gabriel.unionjack@gmail.com>
2023-09-05 16:23:48 -0300

    mais atualizações e movendo as imgs de tecnologias

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,26 @@
+function resolveProfileInfo(profileData){
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `tel:${profileData.phone}`
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `mailto:${profileData.email}`
+} 
+
+function addSoftSkills(profileData){
+    const softSkills = document.getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+
 (async () => {
     const profileData = await fetchProfileData()
-    console.log (profileData)
+    resolveProfileInfo(profileData)
+    addSoftSkills(profileData)
 })()
